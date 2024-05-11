dd30fdb06139cac6cd7ec4efacd95e5246782027
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-05-11 16:16:15 -0400

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -4,54 +4,53 @@ import './navigation-bar.scss';
 
 export const NavigationBar = ({ user, query, movies, handleSearch, onLoggedOut }) => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/">
-          <p>Welome to</p>
-          <h2>MyFlix</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          {!user && (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </>
-            )}
-            {user && (
-              <>
-                <Nav.Link as={Link} to="/">
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to="/profile">
-                  Profile
-                </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
-              </>
-            )}
-            {user && (
-                <Form className="d-flex flex-column">
-                <FormControl
-                  type="search"
-                  placeholder="Searches are case sensitive e.g. Action, Thriller, Comedy, Animation"
-                  className="mr-2"
-                  aria-label="Search"
-                  value={query}
-                  onChange={handleSearch}
-                />
-                <p className="text-muted text-center">
-                  Title and Genre (Action, Thriller, Comedy, Animation) searches are case sensitive.
-                </p>
-                </Form>
-            )}
-          </Nav>    
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+<Navbar bg="dark" variant="dark" expand="lg">
+  <Container fluid>
+    <Navbar.Brand as={Link} to="/">
+      <p>Welome to</p>
+      <h2>MyFlix</h2>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+      <Nav>
+        {!user ? (
+          <>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Signup
+            </Nav.Link>
+          </>
+        ) : (
+          <>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/profile">
+              Profile
+            </Nav.Link>
+            <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+          </>
+        )}
+      </Nav>
+      {user && (
+        <Form className="d-flex flex-column">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="mr-2 mb-2"
+            aria-label="Search"
+            value={query}
+            onChange={handleSearch}
+          />
+          <p className="text-muted text-center">
+            Title and Genre (Action, Thriller, Comedy, Animation) searches are case sensitive.
+          </p>
+        </Form>
+      )}
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 };
